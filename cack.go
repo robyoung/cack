d189c6827ff6acb24161e76401dad173711b24af
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-11-17 21:37:00 +0000

    Extract function to find files.

--- cack.go
+++ cack.go
@@ -8,6 +8,29 @@ import (
 	"regexp"
 )
 
+func findFiles(paths []string) <-chan string {
+	files := make(chan string)
+
+	// Start a go routine to feed the channel
+	go func(files chan <- string, paths []string) {
+		// Walk the filesystem for each path, calling the annonymous
+		// function provided for each subpath.
+		for _, path := range paths {
+			filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+				// Do not send paths if they're directories
+				if !info.IsDir() {
+					files <- path
+				}
+				return nil
+			})
+		}
+		close(files)
+		return
+	}(files, paths)
+
+	return files
+}
+
 func main() {
 	// Verify we have enough arguments and exit if we don't
 	if len(os.Args) < 2 {
@@ -25,29 +48,24 @@ func main() {
 		paths = append(paths, ".")
 	}
 
-	for _, path := range paths {
-		// Walk the filesystem for each path, calling the annonymous
-		// function provided for each subpath.
-		filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
-			// Open the path
-			file, err := os.Open(path)
-			if err != nil {
-				return fmt.Errorf("Failed to open file: %v\n", path)
-			} else {
-				// Read \n terminated lines from the path until there is an error
-				reader := bufio.NewReaderSize(file, 4092)
-				for i := 1; ; i++ {
-					line, err := reader.ReadString('\n')
-					// If a line matches the pattern then print the result.
-					if pattern.MatchString(line) {
-						fmt.Printf("%v:%v:%v", path, i, line)
-					}
-					if err != nil {
-						break
-					}
+	for path := range findFiles(paths) {
+		// Open the path
+		file, err := os.Open(path)
+		if err != nil {
+			fmt.Printf("Failed to open file: %v\n", path)
+		} else {
+			// Read \n terminated lines from the path until there is an error
+			reader := bufio.NewReaderSize(file, 4092)
+			for i := 1; ; i++ {
+				line, err := reader.ReadString('\n')
+				// If a line matches the pattern then print the result.
+				if pattern.MatchString(line) {
+					fmt.Printf("%v:%v:%v", path, i, line)
+				}
+				if err != nil {
+					break
 				}
 			}
-			return nil
-		})
+		}		
 	}
 }
